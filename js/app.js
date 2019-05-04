f864167937e3fdccb38d316cf2b80f8f0b410f16
Katarzyna Paszko <pkatarzynapaszko@gmail.com>
2019-05-04 17:02:24 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+  $(document).ready(function(){
+
+    // zaininicjuj slider slick
+    $('.sliderSlick').slick({
+      infinite: true,
+      slidesToShow: 1,
+      speed: 1000,
+      dots: true,
+      autoplay:true,
+      autoplaySpeed: 5000,
+    });
+
+  });
